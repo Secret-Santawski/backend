63b3aa05f73ecdd1165d63f221d246022ff8da62
WilliamGiorno <70899240+WilliamGiorno@users.noreply.github.com>
2023-11-29 19:43:37 +0000

    Implement /CreateUser/{Id} Fixes #14

--- tests/test_app.py
+++ tests/test_app.py
@@ -3,6 +3,7 @@ import unittest
 from unittest.mock import patch, MagicMock
 from src.app import app
 from models.party_model import Party
+from models.user_model import User
 
 
 class TestApp(unittest.TestCase):
@@ -83,6 +84,34 @@ class TestApp(unittest.TestCase):
 
         # Verify that the response is correct
         self.assertEqual(response.get_json(), {'code': 200, 'message': 'Document updated successfully'})
+        self.assertEqual(response.status_code, 200)
+
+    @patch('src.app.FirebaseCRUD')
+    def test_create_user(self, mock_firebase_crud):
+        # Setup the mock for FirebaseCRUD
+        mock_firebase_crud_instance = mock_firebase_crud.return_value
+        mock_firebase_crud_instance.create.return_value = {'code': 200, 'message': 'Document created successfully', 'id': '123'}
+
+        # Prepare the test data
+        test_data = {
+            'username': 'Test User',
+            'email': 'test@test.com',
+            'ownership': False,
+            'suggested_categories': ['Food', 'Games'],
+            'party_id': 'party123'
+        }
+
+        # Call the create_user endpoint
+        response = self.app.post('/CreateUser/party123', json=test_data)
+
+        # Verify that FirebaseCRUD.create was called with the correct parameters
+        mock_firebase_crud_instance.create.assert_called_once()
+        called_args, _ = mock_firebase_crud_instance.create.call_args
+        self.assertEqual(called_args[0], 'User')
+        self.assertDictEqual(called_args[1], test_data)
+
+        # Verify that the response is correct
+        self.assertEqual(response.get_json(), {'code': 200, 'message': 'Document created successfully', 'id': '123'})
         self.assertEqual(response.status_code, 200)
         
 if __name__ == '__main__':
