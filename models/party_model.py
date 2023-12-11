2aa042cda233bf3629033b33033ccf8d66978c52
WilliamGiorno <70899240+WilliamGiorno@users.noreply.github.com>
2023-12-11 20:22:37 +0000

    Create docker enviroment and refactor endpoints

--- models/party_model.py
+++ models/party_model.py
@@ -2,11 +2,23 @@ from dataclasses import dataclass, field
 from typing import List
 
 @dataclass
+class PartyRequest:
+    name: str
+    budget: int
+    categories: List[str]
+
+    def __post_init__(self):
+        if self.budget < 0:
+            raise ValueError("Budget cannot be negative")
+        if not self.name:
+            raise ValueError("Name cannot be empty")
+
+@dataclass
 class Party:
     name: str
     budget: int
     categories: List[str]
-    ownerId: str
+    ownerId: str = ''
     closed: bool = False
 
     def __post_init__(self):
